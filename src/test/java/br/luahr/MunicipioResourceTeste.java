2fce1b700a1fc3be1c9de5866e7045483cb34ba4
Luahr13 <73291538+Luahr13@users.noreply.github.com>
2023-04-29 20:35:51 -0300

    Teste de creat Municipio ok

--- src/test/java/br/luahr/MunicipioResourceTeste.java
+++ src/test/java/br/luahr/MunicipioResourceTeste.java
@@ -6,12 +6,16 @@ import javax.inject.Inject;
 
 import org.junit.jupiter.api.Test;
 
+import br.luahr.topicos1.dto.EstadoDTO;
+import br.luahr.topicos1.dto.EstadoResponseDTO;
 import br.luahr.topicos1.dto.MunicipioDTO;
 import br.luahr.topicos1.dto.MunicipioResponseDTO;
+import br.luahr.topicos1.service.EstadoService;
 import br.luahr.topicos1.service.MunicipioService;
 import io.quarkus.test.junit.QuarkusTest;
 
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
@@ -21,76 +25,77 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 @QuarkusTest
 public class MunicipioResourceTeste {
 
-    @Inject
-    MunicipioService municipioService;
+        @Inject
+        MunicipioService municipioService;
 
-    @Test
-    public void testGetAll() {
-        given()
-                .when().get("/municipios")
-                .then()
-                .statusCode(200);
-    }
+        @Inject
+        EstadoService estadoService;
 
-    @Test
-    public void testInsert() {
-
-        MunicipioDTO municipioDTO = new MunicipioDTO(
-                "Palmas",
-                1L);
-        given()
-                .contentType(ContentType.JSON)
-                .body(municipioDTO)
-                .when().post("/municipios")
-                .then()
-                .statusCode(201)
-                .body("id", notNullValue(),
-                        "nome", is("Palmas"),
-                                                    "id_estado", is(1L));
-    }
-
-    @Test
-    public void testUpdate() {
-        // Adicionando uma pessoa no banco de dados
-        MunicipioDTO municipioDTO = new MunicipioDTO(
-                "Colinas",
-                1L);
-        Long id = municipioService.create(municipioDTO).id();
-        // Criando outra pessoa para atuailzacao
-        MunicipioDTO municipioUpDTO = new MunicipioDTO(
-                "Palmas",
-                1L);
-        given()
-                .contentType(ContentType.JSON)
-                .body(municipioUpDTO)
-                .when().put("/estados/" + id)
-                .then()
-                .statusCode(204);
-        // Verificando se os dados foram atualizados no banco de dados
-        MunicipioResponseDTO municipioResponseDTO = municipioService.findById(id);
-        assertThat(municipioResponseDTO.nome(), is("Tocantins"));
-        assertThat(municipioResponseDTO.estado().getId(), is(1L));
-    }
-
-    @Test
-    public void testDelete() {
-        // Adicionando uma pessoa no banco de dados
-        MunicipioDTO municipioDTO = new MunicipioDTO(
-                "Palmas",
-                1L);
-        Long id = municipioService.create(municipioDTO).id();
-        given()
-                .when().delete("/estados/" + id)
-                .then()
-                .statusCode(204);
-        // verificando se a pessoa fisica foi excluida
-        MunicipioResponseDTO municipioResponseDTO = null;
-        try {
-            municipioService.findById(id);
-        } catch (Exception e) {
-        } finally {
-            assertNull(municipioResponseDTO);
+        @Test
+        public void testGetAll() {
+                given()
+                                .when().get("/municipios")
+                                .then()
+                                .statusCode(200);
         }
-    }
-    
+
+        @Test
+        public void testeCreatMunicipio() {
+                MunicipioDTO municipioDTO = new MunicipioDTO("Palmas", 1L);
+
+                given()
+                                .contentType(ContentType.JSON)
+                                .body(municipioDTO)
+                                .when().post("/municipios")
+                                .then()
+                                .statusCode(201);
+        }
+
+        @Test
+        public void testUpdate() {
+                // Adicionando uma cidade no banco de dados
+                MunicipioDTO municipioDTO = new MunicipioDTO(
+                                "Palmas",
+                                1L);
+                Long id = municipioService.create(municipioDTO).id();
+
+                // Criando outro cidade para atuailzacao
+                MunicipioDTO municipioUpDTO = new MunicipioDTO(
+                                "Palmas_TO",
+                                2L);
+
+                given()
+                                .contentType(ContentType.JSON)
+                                .body(municipioUpDTO)
+                                .when().put("/municipios/" + id)
+                                .then()
+                                .statusCode(204);
+
+                // Verificando se os dados foram atualizados no banco de dados
+                MunicipioResponseDTO municipioResponseDTO = municipioService.findById(id);
+                assertThat(municipioResponseDTO.nome(), is("Palmas"));
+                assertThat(municipioResponseDTO.estado(), is(2L));
+        }
+
+        @Test
+        public void testDelete() {
+                // Adicionando uma pessoa no banco de dados
+                MunicipioDTO municipioDTO = new MunicipioDTO(
+                                "Palmas",
+                                1L);
+                Long id = municipioService.create(municipioDTO).id();
+                given()
+                                .when().delete("/estados/" + id)
+                                .then()
+                                .statusCode(204);
+                // verificando se a pessoa fisica foi excluida
+                MunicipioResponseDTO municipioResponseDTO = null;
+                try {
+                        municipioService.findById(id);
+                } catch (Exception e) {
+                } finally {
+                        assertNull(municipioResponseDTO);
+                }
+        }
+
 }
